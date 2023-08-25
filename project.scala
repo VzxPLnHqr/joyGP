eadd910fc71c1bd756a38323e1527939afd1120e
VzxPLnHqr <VzxPLnHqr@proton.me>
2023-08-25 19:46:21 +0000

    some project deps

---
+++ project.scala
@@ -0,0 +1,4 @@
+//> using lib "org.typelevel::cats-effect:3.5.1"
+//> using lib "org.typelevel::cats-core:2.10.0"
+//> using lib "com.fiatjaf::scoin:0.7.0"
+//> using lib "org.typelevel::spire:0.18.0"
