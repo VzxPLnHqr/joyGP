9a7646a56e09cf876f52d9d473ccb744e7b5d7a0
VzxPLnHqr <VzxPLnHqr@proton.me>
2023-09-02 06:18:06 +0000

    broken fitness

--- examples/AdditionModP.scala
+++ examples/AdditionModP.scala
@@ -64,17 +64,14 @@ trait AdditionModP:
     val inputStackSizeDiff = math.abs(expected.input.size - candidate.input.size).toDouble
     val outputSizeDiff = math.abs(expected.output.size - candidate.output.size)
     val outputValueDiff = if(outputSizeDiff == 0)
-                            math.abs(
-                              expected.output.toInt(false,scodec.bits.ByteOrdering.BigEndian) -
-                              candidate.output.toInt(false,scodec.bits.ByteOrdering.BigEndian)
-                            )
+                              expected.output.xor(candidate.output).toInt(false,scodec.bits.ByteOrdering.LittleEndian).toDouble
                           else
-                            Int.MaxValue - 1
+                              math.pow(2.0,32) - 1
     List(
       math.log(Int.MaxValue.toDouble - execStackSizeDiff),
       math.log(Int.MaxValue.toDouble - inputStackSizeDiff),
       math.log(Int.MaxValue.toDouble - outputSizeDiff),
-      math.log(Int.MaxValue.toDouble - outputValueDiff) // if output size not same, this component is zero
+      math.log(math.pow(2.0,32) - outputValueDiff).ensuring(_ > 0) // if output size not same, this component is zero
     ).sum
   }.ensuring(_ > 0)
 
@@ -82,14 +79,19 @@ trait AdditionModP:
 
   val randomIO: IO[std.Random[IO]] = std.Random.scalaUtilRandom
 
-  val startingPop: IO[List[ScoredIndividual[Double]]] = List.range(0,100).parTraverse{ i => 
+  val bestYet = IO(
+    "33175f775b45d3f9e776c173ddee3ff67234c6dadb9428eec4d36cf43830ef3e26b72f1bbe461afedae115d3979fcb04a16676ae42866f36dbe2980bfea822fa1707fb5b2056a28d2d2782fcc2ada2036fab87965b96c84afc9b97d7bd291a86f76831fae8d6cef1019e4f3f45461a06d577ea3be888fd1937a51a7ff7f05797d3a64f8de28610e4681b08cda9bfc9b6ca195df69cd0dcd54dbf8de48aa7b4bf83372280863867a4e7db171e9fd73c8df7013b64590cc7640de4e26c09ae9a3d3e0e6b1bcd8a0355d03be53357e0e608ae9a353e2e78376fa88c678a751ffa8ee0fe5be647d316dbc97c152fdd6a5aaad4608"
+  ).map(hex => BitVector.fromValidHex(hex)).flatMap(bits => fitness(Program.parse(bits)).map(score => ScoredIndividual(bits,score)))
+  
+  val startingPop = bestYet.map(best => List.fill(100)(best))
+  /*val startingPop: IO[List[ScoredIndividual[Double]]] = List.range(0,100).parTraverse{ i => 
     for {
       size <- randomIO.flatMap(_.betweenInt(5,100))
       bits <- randomIO.flatMap(_.nextBytes(size)).map(BitVector(_))
       indiv <- IO(Program.parse(bits))
       score <- fitness(indiv)
     } yield ScoredIndividual(bits,score)
-  }
+  }*/
 
   // evolve n generations
   def evolve(n: Int, numParallel: Int, printEvery: Int = 10, maxTarget: Option[Double] = None)
