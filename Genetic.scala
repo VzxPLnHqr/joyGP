60e3ffe3a50ed4f500f55ce3fbf7fbc76d343cee
VzxPLnHqr <VzxPLnHqr@proton.me>
2023-09-15 04:51:36 +0000

    monad methods for Genetic

--- Genetic.scala
+++ Genetic.scala
@@ -3,7 +3,7 @@ package joygp
 import scodec.bits._
 
 
-trait Genetic[A]{
+trait Genetic[A]:
     /**
       * construct a value of type `A` from
       * genome represented as bitvector
@@ -11,5 +11,24 @@ trait Genetic[A]{
       * @param genome
       * @return
       */
-    def fromBits(genome: BitVector): A
-}
+    def fromBits(genome: BitVector): Genetic.Decoded[A]
+
+object Genetic:
+  final case class Decoded[A](value: A, remainingGenome: BitVector)
+
+
+  def point[A](a: A): Genetic[A] = new Genetic[A] {
+    def fromBits(genome: BitVector): Decoded[A] = Decoded(a,genome)
+  }
+
+  def fmap[A,B](ga: Genetic[A])(f: A => Genetic[B]): Genetic[B] = new Genetic[B] {
+    def fromBits(genome: BitVector): Decoded[B] =
+      val Decoded(a,remainingGenome) = ga.fromBits(genome)
+      f(a).fromBits(remainingGenome)
+  }
+
+  def map[A,B](ga: Genetic[A])(f: A => B): Genetic[B] = new Genetic[B] {
+    def fromBits(genome: BitVector): Decoded[B] =
+      val Decoded(a,remaining) = ga.fromBits(genome)
+      Decoded(f(a),remaining)
+  }
